dcf8719fbc539a3727c2152e536a189f3c596059
yubin <qkrdbqls1001@naver.com>
2020-06-08 18:42:37 +0900

    메인 루프 설정

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ pg.init()
 WIDTH, HEIGHT = c.size['SCREEN_SIZE']
 pg.display.set_caption("총알 피하기")
 screen = pg.display.set_mode(c.size['SCREEN_SIZE'])
+
+# Game Loop
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+    
+    screen.fill(c.color['black'])
+
+    pg.display.update()
