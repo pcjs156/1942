7c245e67c71763d2a868620fa0f9fb8f30e982db
yubin <qkrdbqls1001@naver.com>
2020-06-16 16:17:52 +0900

    player.py 주석 처리 완료

--- player.py
+++ player.py
@@ -6,7 +6,17 @@ import math
 
 from const import Const as c
 
+
+# 이상 모듈/클래스 import 
+# ###################################################################
+# 이하 Player 클래스
+
+
 class Player:
+    # ###################################################################
+    # 이하 Player 클래스 기본 설정
+
+    # 플레이어 기본 속도
     SPEED = 30
     # 미션 2 수정 : Player 이미지를 폭발 이미지로 바꾸는 코드를 main.py에서 player.py로 옮김 
     boom_image = pg.image.load('resource/img/flame.png')
@@ -16,35 +26,44 @@ class Player:
     # 색반전 이미지, 비 색반전 이미지를 분리하여 로드
     non_negative_image = pg.image.load('resource/img/player.png')
     non_negative_image = pg.transform.scale(non_negative_image, c.size['PLAYER_SIZE'])
-
     negative_image = pg.image.load('resource/img/player_negative.png')
     negative_image = pg.transform.scale(negative_image, c.size['PLAYER_SIZE'])
 
+
     def __init__(self, x, y):
+        # 처음엔 색반전되지 않은 이미지로 시작
         self.image = Player.non_negative_image
+
         # 좌표는 각각 [x, y]좌표
         self.pos = [x, y] # 플레이어의 현재 위치(update에 의해 갱신됨)
         self.to = [0, 0] # 플레이어가 이동할 방향(상, 하, 좌, 우, ...)
-        self.angle = 0
+        self.angle = 0 # 플레이어 각도
 
         # HP
         self.HP = 5        
-        self.attacked_time = 0
-        # 무적 시간 : FPS * 5초
-        self.invincible_time = 0
-        self.is_invincible = False
+        # 무적 현재 무적 시간인지 검사하기 위한 변수
+        self.attacked_time = 0 # 피격당한 시각(초 단위)
+        self.is_invincible = False # 현재 무적 상태?
 
-        # 색반전으로 무적시간동안 플레이어가 반짝거리도록 조정
+        # 색반전으로 무적시간동안 플레이어가 반짝거리도록 조정(스위칭)
         self.now_negative_image = False
         
+
+    # 이상 Player 클래스 기본 설정   
+    # ###################################################################
+    # 이하 기능 구현 : 렌더링/위치 관련
+
+
     # 화면에 플레이어를 렌더링
     def draw(self, screen):
-        # 죽은 경우
-        # HP가 1씩 떨어지는게 아니기 때문에 조건을 변경함
+        # 죽은 경우(HP가 0보다 낮아질 수도 있음)
         if self.HP <= 0:
+            # 플레이어의 이미지를 폭발하는 이미지로 변경
             self.image = Player.boom_image
+
         # 무적인 경우
         elif self.is_invincible is True:
+            # self.now_negative_image 플래그를 깜빡거리며 색반전/원래 색 플레이어 이미지를 전환
             if self.now_negative_image:
                 self.image = Player.non_negative_image
                 self.now_negative_image = False
@@ -52,7 +71,9 @@ class Player:
                 self.image = Player.negative_image
                 self.now_negative_image = True
 
-        # 무적이 아닌 경우    
+        # 무적이 아닌 경우
+        # 만약 non_negative_image가 False에 '걸칠'경우 색이 반전되어 플레이어 이미지가 고정됨
+        # 따라서 무적이 아닌 경우 항상 플레이어의 이미지를 색이 반전되지 않은 이미지로 고정 
         else :
             self.image = Player.non_negative_image
 
@@ -70,11 +91,16 @@ class Player:
 
         calib_pos = (self.pos[0] - c.size['PLAYER_WIDTH']/2,
                      self.pos[1] - c.size['PLAYER_HEIGHT']/2)
+
+        # 화면에 렌더링
         screen.blit(rotated_image, calib_pos)
 
+
+    # 플레이어의 방향 설정
     def goto(self, x, y):
         self.to[0] += x
         self.to[1] += y
+
 
     # 화면상의 플레이어 위치를 업데이트
     def update(self, dt):
@@ -85,12 +111,19 @@ class Player:
         self.pos[0] = min(max(self.pos[0], c.size['PLAYER_WIDTH']), c.size['SCREEN_WIDTH']-c.size['PLAYER_WIDTH'])
         self.pos[1] = min(max(self.pos[1], c.size['PLAYER_HEIGHT']), c.size['SCREEN_HEIGHT']-c.size['PLAYER_HEIGHT'])
     
+
+    # 이상 기능 구현 : 렌더링/위치 관련
+    # ###################################################################
+    # 이하 기능 구현 : 충돌 감지/처리 관련
+
+
     # 충돌 감지 함수
     def collision(self, bullet):
         if math.sqrt((self.pos[0] - bullet.pos[0])**2 +
                     (self.pos[1] - bullet.pos[1])**2) < 20 :
             return True
         return False
+
 
     # * 피격당했을 경우 
     # 1.플레이어의 HP를 총알의 데미지만큼 깎음
@@ -109,3 +142,11 @@ class Player:
         if time.time() - self.attacked_time >= c.INVINCIBLE:
             # 따라서 무적상태 해제
             self.is_invincible = False
+
+
+    # 이상 기능 구현 : 충돌 감지/처리 관련
+    # ###################################################################
+
+
+# 이상 Player 클래스 
+# ###################################################################
