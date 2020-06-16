d3ca2655e796b9ea7dff63bdc25c71e4fe23d155
yubin <qkrdbqls1001@naver.com>
2020-06-16 16:02:47 +0900

    const.py 주석처리 완료

--- const.py
+++ const.py
@@ -50,7 +50,11 @@ class Const:
         'LEFT':[-1, 0],
         'RIGHT':[1, 0],
         'DOWN':[0, 1],
-        'UP':[0, -1]
+        'UP':[0, -1],
+        'LEFTUP':[-1, -1],
+        'RIGHTUP':[1, -1],
+        'LEFTDOWN':[-1, 1],
+        'RIGHTDOWN':[1, 1]
     }
     
     
