82175c7f025b2d6047b511fe08312ad9548edb96
yubin <qkrdbqls1001@naver.com>
2020-06-16 14:13:00 +0900

    const.py 정리 / 주석처리

--- const.py
+++ const.py
@@ -1,10 +1,15 @@
-class Const:
-    FPS = 60
-    INVINCIBLE = 0.5
+# 전 파일에 거쳐 사용할 변수를 관리하는 클래스
 
-    # 1이 기본, 클수록 어려움(초당 총알 갯수)
+class Const:
+    # FPS
+    FPS = 60
+    # 무적시간
+    INVINCIBLE = 2
+
+    # 초당 생성되는 새 총알의 갯수
     DIFFICULTY = 1
 
+    # RGB코드
     color = {
         'BLACK':(0, 0, 0),
         'WHITE':(255, 255, 255),
@@ -13,6 +18,7 @@ class Const:
         'BLUE':(0, 0, 255),
         'GREY':(224, 224, 224),
 
+        # 배터리 칸에 각각 사용될 RGB코드
         'BATTERY':[(239,69,54),
             (251,176,64),
             (248,237,60),
@@ -21,32 +27,30 @@ class Const:
         ][::-1]
     }
     
+    # 객체 크기
     size = {
+        # 메인 화면
         'SCREEN_WIDTH':1000,
         'SCREEN_HEIGHT':800,
         'SCREEN_SIZE':(1000, 800),
 
+        # 플레이어
         'PLAYER_WIDTH':64,
         'PLAYER_HEIGHT':64,
         'PLAYER_SIZE':(64, 64),
 
+        # 총알
         'BULLET_RADIUS':7,
         'WEAK_BULLET_RADIUS':16,
         'NORMAL_BULLET_RADIUS':8,
         'STRONG_BULLET_RADIUS':4,
-        'SCOREBOARD_SIZE':32,
-        'GAMEOVER_SIZE':100
     }
 
     vector = {
         'LEFT':[-1, 0],
         'RIGHT':[1, 0],
         'DOWN':[0, 1],
-        'UP':[0, -1],
-        'LEFTUP':[-1, -1],
-        'RIGHTUP':[1, -1],
-        'LEFTDOWN':[-1, 1],
-        'RIGHTDOWN':[1, 1]
+        'UP':[0, -1]
     }
     
     
